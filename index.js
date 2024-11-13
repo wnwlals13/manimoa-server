bc0bb833e6e5ff6353c63e2459ef89d518bcf471
wnwlals13 <wnwlals13@naver.com>
2024-11-13 21:01:56 +0900

    241113 커밋 메세지 수정

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -13,27 +14,26 @@ const port = process.env.PORT || 5000;
 
 // socket이 연결되었을 때
 io.on("connection", (socket) => {
-  console.log("채팅 연결");
+  console.log("소켓 연결");
 
   // n번 채팅방 입장
   const roomIndex = socket.handshake.query.chatRoomId;
-  socket.on("joinChatRoom", (userId) => {
+  socket.on("joinRoom", (roomId, userId) => {
     socket.userId = userId;
-    socket.join(roomIndex);
-    socket.room = roomIndex;
-    socket.to(roomIndex).emit("join", roomIndex + "chatRoom join");
-
-    console.log(`${roomIndex} 번 방에 ${userId} 사용자가 입장했습니다.`);
+    socket.join(roomId);
+    socket.room = roomId;
+    socket.to(roomId).emit("join", roomId + "chatRoom join");
+    console.log(`${userId}님이 ${roomIndex} 번 방에 입장했습니다.`);
   });
 
   socket.on("chatting", (data) => {
     io.in(data.roomId).emit("send message", data); // roomIndex 방에 채팅 emit = 방출
-    console.log(`${roomIndex} 번 방에 사용자가 메세지를 전송했씁니다.`);
+    console.log(`${roomIndex} 번 방에 사용자가 메세지를 전송했습니다.`);
   });
 
   // disconnect
   socket.on("disconnect", () => {
-    console.log("채팅 연결 해제");
+    console.log("소켓 연결 해제", socket.rooms);
   });
 });
 
