850129754379062258020d250eebeeb9131e94d9
wnwlals13 <wnwlals13@naver.com>
2024-11-14 15:27:40 +0900

    fix:241114 origin 추가

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = require("http").createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["https://dev-manimoa.vercel.app"],
+    origin: ["https://dev-manimoa.vercel.app", "http://localhost:3000"],
     credentials: true,
   },
 });
